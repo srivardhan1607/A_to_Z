080a8c39fb2ed372d2f8626e2732d15e21cc0539
srivardhan1607 <85606850+srivardhan1607@users.noreply.github.com>
2023-06-02 19:01:24 +0530

    initial

--- force-app/main/default/lwc/userSetupPage/userSetupPage.js
+++ force-app/main/default/lwc/userSetupPage/userSetupPage.js
@@ -1,5 +1,49 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track } from 'lwc';
 
 export default class UserSetupPage extends LightningElement {
-    
+    @track startHours = {
+        monday: '',
+        tuesday: '',
+        wednesday: '',
+        thursday: '',
+        friday: '',
+        saturday: '',
+        sunday: ''
+    };
+
+    @track endHours = {
+        monday: '',
+        tuesday: '',
+        wednesday: '',
+        thursday: '',
+        friday: '',
+        saturday: '',
+        sunday: ''
+    };
+
+    daysOfWeek = [
+        { label: 'Monday', value: 'monday' },
+        { label: 'Tuesday', value: 'tuesday' },
+        { label: 'Wednesday', value: 'wednesday' },
+        { label: 'Thursday', value: 'thursday' },
+        { label: 'Friday', value: 'friday' },
+        { label: 'Saturday', value: 'saturday' },
+        { label: 'Sunday', value: 'sunday' }
+    ];
+
+    handleStartChange(event) {
+        const day = event.target.dataset.day;
+        this.startHours[day] = event.target.value;
+    }
+
+    handleEndChange(event) {
+        const day = event.target.dataset.day;
+        this.endHours[day] = event.target.value;
+    }
+
+    handleSave() {
+        // Perform save logic here
+        console.log('Start Hours:', this.startHours);
+        console.log('End Hours:', this.endHours);
+    }
 }
